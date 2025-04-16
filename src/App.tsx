b6c88a2e4ab760c890b68ebd12a33af047770e19
Ilyas <ilyas.lachgar@ecoles-epsi.net>
2025-04-17 01:25:10 +0200

    added navbar component with styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Global.css';
+import Navbar from './components/navbar/navbar';
 import AppRouter from './routes/Router';
 import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
     <AuthProvider>
-      <AppRouter />
+      <div className="pt-20">
+        <Navbar />
+        <AppRouter />
+      </div>
     </AuthProvider>
   );
 }
